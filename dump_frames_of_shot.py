c0d7bdee8ed9aba9c474d97cd2b5ce6b27e7608b
Xavier Puig <xavierpuigf@gmail.com>
2018-07-23 22:45:32 -0400

    dump frames of shot - no imdb key

--- dump_frames_of_shot.py
+++ dump_frames_of_shot.py
@@ -5,7 +5,6 @@ import numpy as np
 # wrapper for FFMPEG through OpenCV
 import cv2
 # Local imports
-from config import PROC_BASE
 
 
 def read_videvents(fname):
@@ -23,12 +22,12 @@ def read_videvents(fname):
     return events
 
 
-def main(imdb_key, video_fname):
+def main(video_name, video_fname):
     """Load videvents, convert, loop through frames, and write images to disk, etc.
     """
 
     # get list of events
-    videvents = read_videvents(os.path.join(PROC_BASE, imdb_key, imdb_key + '.videvents'))
+    videvents = read_videvents(os.path.join(args.base_dir, video_name, video_name + '.videvents'))
     # open the video
     video = cv2.VideoCapture(video_fname)
     assert video.isOpened(), 'Failed to open the video!'
@@ -41,7 +40,7 @@ def main(imdb_key, video_fname):
     frames_to_write['begin'] =  begin
 
     frames_to_write['end'] = (np.array(videvents) - 1).tolist()  # get last-frames of each shot
-    frames_to_write['end'].append(int(video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT) - 1))  # add last frame of video
+    frames_to_write['end'].append(int(video.get(cv2.CAP_PROP_FRAME_COUNT) - 1))  # add last frame of video
 
     # midframes
     frames_to_write['mid'] = ((np.array(frames_to_write['begin']) +  np.array(frames_to_write['end']))/2).tolist()
@@ -49,7 +48,7 @@ def main(imdb_key, video_fname):
     # create folder mappings and the folders
     dir_map = {'begin': 'start_frame', 'mid': 'mid_frame', 'end': 'last_frame'}
     for dirname in dir_map.values():
-        os.makedirs(os.path.join(PROC_BASE, imdb_key, dirname))
+        os.makedirs(os.path.join(args.base_dir, video_name, dirname))
 
     # start writing frames of each shot
     fn = 0
@@ -61,28 +60,32 @@ def main(imdb_key, video_fname):
         for key in frames_to_write.keys():
             if fn in frames_to_write[key]:
                 shot = frames_to_write[key].index(fn)
-                im_filename = os.path.join(PROC_BASE, imdb_key, dir_map[key], 'shot-%04d.fn-%06d.png' %(shot, fn))
+                im_filename = os.path.join(args.base_dir, video_name, dir_map[key], 'shot_%04d_fn_%06d.jpg' %(shot, fn))
                 cv2.imwrite(im_filename, frame)
 
         fn += 1
 
 
 parser = argparse.ArgumentParser(description='Process video file inputs')
-parser.add_argument('video_fname', type=str, help='Video file path')
-parser.add_argument('imdb_key', type=str, help='IMDb key')
+parser.add_argument('--video_fname', type=str, help='Video file path')
+parser.add_argument('--imdb_key', type=str, help='IMDb key')
+parser.add_argument('--base_dir', type=str, help='Base directory')
 
 if __name__ == '__main__':
     # get arguments
     args = parser.parse_args()
-    assert args.imdb_key.startswith('tt'), 'Invalid IMDb key'
-
+    if args.imdb_key is not None:
+        assert args.imdb_key.startswith('tt'), 'Invalid IMDb key'
+        movie_name = args.imdb_key
+    else:
+        movie_name = '.'.join(args.video_fname.split('/')[-1].split('.')[:-1])
     # if video_fname has .720p.mp4 at the end, look for using full-res video without the .720p
     fname = args.video_fname
     if fname.endswith('.720p.mp4') and os.path.exists(fname.replace('.720p.mp4', '.mp4')):
         fname = fname.replace('.720p.mp4', '.mp4')
 
-    print "Working on", args.imdb_key
-    print "Video:", fname
-    main(args.imdb_key, fname)
+    print("Working on", movie_name)
+    print("Video:", fname)
+    main(movie_name, fname)
 
 
