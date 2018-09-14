9acd18547967db2ce1a86b95a35a35f4365f9b9a
xavierpuigf <xavierpuigf@gmail.com>
2018-09-14 10:32:27 -0400

    Modified shell script

--- run_shot_detection.sh
+++ run_shot_detection.sh
@@ -1,7 +1,10 @@
 video_path="$1"
 out_dir="$2"
-moviefile=$(basename "$video_path")
-out_dfd_file="${out_dir}/${moviefile}.dfd"
+moviefile_base=$(basename "$video_path")
+moviefile=$(echo "$moviefile_base" | sed -e 's/\.[^.]*$//')
+movie_path="${out_dir}/${moviefile}/"
+mkdir $movie_path
+out_dfd_file="${movie_path}/${moviefile}.dfd"
 ./ShotDetect "$video_path" "$out_dfd_file"
 python mat_index.py --video_fname "$video_path" --base_dir "$out_dir"
 python dfd_to_videoevents.py --base_dir "$out_dir" --dfd_path "$out_dfd_file"
